10fe60798f495e87b1f07ff0120bf9bf9abedae2
Kate Bolshakova <katassr@mail.ru>
2023-02-14 14:41:05 +0300

    feat: change index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = []
+  for (let i = 0; i < str.length; i++) {
+    let x = str[i]
+    for (let j = 0; j < bracketsConfig.length; j++) {
+      
+      let open = bracketsConfig[j][0]
+      let cloze = bracketsConfig[j][1]
+      if (cloze === x  && stack.includes(open)) {
+        stack.pop()
+        break
+        } else if (open === x || cloze === x) {
+        stack.push(x)
+        break
+      }
+      }
+    } 
+    return stack.length === 0
 }
